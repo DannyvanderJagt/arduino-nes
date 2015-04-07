44762ee19fdb5afa9a8670a342fe384573452a02
Danny van der Jagt <dannyvanderjagt@gmail.com>
2015-04-07 10:38:49 +0200

    Minor Updates and fixes.

---
+++ examples/index.js
@@ -0,0 +1,37 @@
+var Nes = require('./../lib');
+
+var nes = new Nes({
+	serial: "/dev/tty.usbmodem1d1111",
+	controllers:[
+		{
+			clock: 3,
+			latch: 2,
+			data: 4
+		},
+		{
+			clock: 6,
+			latch: 5,
+			data: 7
+		}
+	]
+});
+
+nes.on('error', function(){
+	console.log('error', arguments);
+}).on('ready', function(){
+	console.log('ready', arguments);
+}).on('connected', function(){
+	console.log('connected', arguments);
+}).on('disconnect', function(){
+	console.log('disconnect', arguments);
+});
+
+// The first controller.
+nes.controller[0].on('*', function(event){
+	console.log('controller 0:', event);
+})
+
+// The second controller.
+nes.controller[1].on('*', function(event){
+	console.log('controller 1:', event);
+})
